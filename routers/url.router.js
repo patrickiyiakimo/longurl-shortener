6914331fb4bb3372aadd40cbf1012672caefbbe0
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-05 15:00:07 +0100

    changed the api response

--- routers/url.router.js
+++ routers/url.router.js
@@ -12,11 +12,7 @@ router.post("/urlSubmit", async (req, res) => {
     });
     await urlSubmit.save();
 
-    res
-      .status(200)
-      .json({
-        shortUrl: `https://longurl-shortener-server.vercel.app/${randomUrl}`,
-      });
+    res.status(200).json({ shortUrl: `http://localhost:8000/${randomUrl}` });
   } catch (error) {
     console.error(error.message);
     res.status(400).send("Error while shortening URL");
